db56978bd3cc577cf3732a925f295dec06584af3
siddhika9499 <siddhika.pujari23@pccoepune.org>
2026-08-10 12:16:02 +0530

    Time: 13 ms (39.92%), Space: 32 MB (87.23%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int i=0;
+        int j=1;
+        int sum = 0;
+        while(j<=nums.size()){
+            sum+=min(nums[i],nums[j]);
+            i +=2;
+            j +=2;
+        }
+        return sum;
+    }
+};
